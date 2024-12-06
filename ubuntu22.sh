b36754647dbfcbb72761a0764bca9f4e296c196d
Fábio Henrique Araújo <contato@fabioharaujo.com.br>
2024-12-06 11:18:48 +0000

    Criado 22 e alterado 24 para não tentar pegar sudo, pois já faço manual

--- ubuntu22.sh
+++ ubuntu22.sh
@@ -2,7 +2,7 @@
 
 # Baixa o pacote de instalação do repositório Zabbix
 echo "Baixando o pacote de instalação do repositório Zabbix..."
-wget -q https://repo.zabbix.com/zabbix/7.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_latest_7.0+ubuntu24.04_all.deb
+wget -q https://repo.zabbix.com/zabbix/7.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_latest_7.0+ubuntu22.04_all.deb
 
 # Instala o pacote de repositório do Zabbix
 echo "Instalando o pacote de repositório do Zabbix..."
@@ -10,11 +10,11 @@ dpkg -i zabbix-release_latest_7.0+ubuntu24.04_all.deb
 
 # Atualiza os pacotes
 echo "Atualizando os pacotes do sistema..."
-apt update -y
+apt update
 
 # Instala o agente Zabbix e plugins
 echo "Instalando o Zabbix Agent 2 e plugins..."
-apt install -y zabbix-agent2 zabbix-agent2-plugin-*
+apt install zabbix-agent2 zabbix-agent2-plugin-*
 
 # Altera a configuração do Zabbix Agent 2
 CONFIG_FILE="/etc/zabbix/zabbix_agent2.conf"
